16e600dae16c96dea4cdbf94b36da75c0d9903b3
Matilda Mared <matildamared@live.se>
2022-09-26 09:57:30 +0200

    add failing test

--- src/BowlingScoreCalculator/bowlingScoreCalculator.test.ts
+++ src/BowlingScoreCalculator/bowlingScoreCalculator.test.ts
@@ -1,7 +1,7 @@
 import { BowlingScoreCalculator } from "./BowlingScoreCalculator";
 
 describe("Bowling Score Calculator", () => {
-	it("calculates score to 300 points given the throws X X X X X X X X X X X X", () => {
+	it("calculates score to 300 points given the throws 'X X X X X X X X X X X X'", () => {
 		const throws = "X X X X X X X X X X X X";
 		const throwsIterable = throws.split(" ");
 		const bowlingScoreCalculator = new BowlingScoreCalculator("Test");
@@ -16,7 +16,7 @@ describe("Bowling Score Calculator", () => {
 		expect(result).toBe(expected);
 	});
 
-	it("caculates score to 150 points given the throws 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5", () => {
+	it("caculates score to 150 points given the throws '5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5'", () => {
 		const throws = "5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5";
 		const throwsIterable = throws.split(" ");
 		const bowlingScoreCalculator = new BowlingScoreCalculator("Test");
@@ -26,6 +26,21 @@ describe("Bowling Score Calculator", () => {
 		}
 
 		const expected = 150;
+		const result = bowlingScoreCalculator.totalScore;
+
+		expect(result).toBe(expected);
+	});
+
+	describe("calculates score to 90 points given the throws '9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 -'", () => {
+		const throws = "9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 - 9 -";
+		const throwsIterable = throws.split(" ");
+		const bowlingScoreCalculator = new BowlingScoreCalculator("Test");
+
+		for (const bowlingThrow of throwsIterable) {
+			bowlingScoreCalculator.addThrow(bowlingThrow);
+		}
+
+		const expected = 90;
 		const result = bowlingScoreCalculator.totalScore;
 		bowlingScoreCalculator.announce();
 		console.log(bowlingScoreCalculator.bowlingThrows);
