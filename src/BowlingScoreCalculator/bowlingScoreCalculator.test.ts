e3b08b0a9ace0b6417954a80d62a62e4b530515f
Matilda Mared <matildamared@live.se>
2022-09-26 07:38:29 +0200

    add failing test

--- src/BowlingScoreCalculator/bowlingScoreCalculator.test.ts
+++ src/BowlingScoreCalculator/bowlingScoreCalculator.test.ts
@@ -1,7 +1,7 @@
 import { BowlingScoreCalculator } from "./BowlingScoreCalculator";
 
 describe("Bowling Score Calculator", () => {
-	it("calculates total to 300 points given the throws X X X X X X X X X X X X", () => {
+	it("calculates score to 300 points given the throws X X X X X X X X X X X X", () => {
 		const throws = "X X X X X X X X X X X X";
 		const throwsIterable = throws.split(" ");
 		const bowlingScoreCalculator = new BowlingScoreCalculator("Test");
@@ -11,6 +11,21 @@ describe("Bowling Score Calculator", () => {
 		}
 
 		const expected = 300;
+		const result = bowlingScoreCalculator.totalScore;
+
+		expect(result).toBe(expected);
+	});
+
+	it("caculates score to 150 points given the throws 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5", () => {
+		const throws = "5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5 / 5";
+		const throwsIterable = throws.split(" ");
+		const bowlingScoreCalculator = new BowlingScoreCalculator("Test");
+
+		for (const bowlingThrow of throwsIterable) {
+			bowlingScoreCalculator.addThrow(bowlingThrow);
+		}
+
+		const expected = 150;
 		const result = bowlingScoreCalculator.totalScore;
 
 		expect(result).toBe(expected);
